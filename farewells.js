0d9ffb60bd54a4dc53db1311d5afbea75e6c12ee
Mariel <mariel.capulli@gmail.com>
2018-05-20 12:53:19 -0400

    Basic Node App Assignment

---
+++ farewells.js
@@ -0,0 +1,11 @@
+exports.farewellEnglish = function () {
+    console.log("Goodbye");
+}
+
+exports.farewellSpanish = function () {
+    console.log("Adios");
+}
+
+exports.farewellItalian = function () {
+    console.log("Ciao");
+}
---
+++ main.js
@@ -0,0 +1,16 @@
+const greetings = require("./greeting.js");
+const farewell = require("./farewells.js");
+
+greetings.greetingEnglish();
+
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+farewell.farewellEnglish();
+
+farewell.farewellSpanish();
+
+farewell.farewellItalian();
